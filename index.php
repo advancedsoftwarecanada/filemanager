69c66ff192ffeecb911366bd97f078c02c442e9f
Andy Normore <andrewnormore@gmail.com>
2013-03-05 11:36:36 -0500

    index.php: Changed interface for practical useage.
    lib.php: Appropriate code was moved here
    
    view.php: To showcase how to pull files from external pages

--- index.php
+++ index.php
@@ -5,12 +5,13 @@
 //
 // ===============
 // @Author: Andy Normore
+// @Author: Davo Smith  
 // https://github.com/AndyNormore/MoodleFileManager
 
 // The point of this file is to demonstrate how to manage files within Moodle 2.3
 // Why? Because file management is incredibly hard for some reason.
 // This file is built to run as STANDALONE, no external files or strings. Just 100% easy to understand! (Noob friendly)
-
+// Thanks to Davo Smith for helping to create this project. 
 
 
 
@@ -22,62 +23,26 @@
 //
 // --------------
 require_once( '../../config.php' );
-global $CFG, $USER, $DB, $OUTPUT;
+global $CFG, $USER, $DB, $OUTPUT, $PAGE;
 
 $PAGE->set_url('/local/filemanager/index.php');
 require_login();
 
 $PAGE->set_pagelayout( 'admin' );
-$context = get_context_instance( CONTEXT_COURSE, SITEID );
+
+// Choose the most appropriate context for your file manager - e.g. block, course, course module, this example uses
+// the system context (as we are in a 'local' plugin without any other context)
+// This is VERY important, the filemanager MUST have a valid context!
+$context = context_system::instance();
 $PAGE->set_context( $context );
 
-$PAGE->set_title( 'Page Title' );
-$PAGE->set_heading( 'Page Heading' );
-	
+// Setup the page
+$PAGE->set_title( 'File Manager Example' );
+$PAGE->set_heading( 'File Manager Example' );
+
 //DEFINITIONS
 require_once($CFG->libdir.'/formslib.php');
-require_once($CFG->libdir.'/completionlib.php');
-
-
-
-
-
-
-
-// ===============
-//
-//
-//	(LIBRARY) this would usually be in an: require('lib.php');
-//
-//
-// ===============
-
-class simplehtml_form extends moodleform {
-
-    function definition() {
-        global $CFG;
- 
-        $mform = $this->_form; // Don't forget the underscore! 
-			
-		// FILE MANAGER
-		$mform->addElement('filemanager', 'attachments', 'File Manager label', null, array('subdirs' => 0, 'maxbytes' => 0, 'maxfiles' => 50, 'accepted_types' => array('*') ));
-		
-		// Buttons
-		$this->add_action_buttons();
-	
-    }
-    
-    function validation($data, $files) {
-        return array();
-    }
-}
-
-
-
-
-
-
-
+require_once('lib.php');
 
 
 
@@ -85,30 +50,38 @@ class simplehtml_form extends moodleform {
 // ===============
 //
 //
-//	PAGE LOGIC
+// PAGE LOGIC
 //
 //
 // ===============
 
-$mform = new simplehtml_form();
+// Create some options for the file manager
+$filemanageropts = array('subdirs' => 0, 'maxbytes' => '0', 'maxfiles' => 50, 'context' => $context);
+$customdata = array('filemanageropts' => $filemanageropts);
 
+// Create a new form object (found in lib.php)
+$mform = new simplehtml_form(null, $customdata);
 
-
+// ---------
 // CONFIGURE FILE MANAGER
+// ---------
 // From http://docs.moodle.org/dev/Using_the_File_API_in_Moodle_forms#filemanager
-if (empty($entry->id)) {
-    $entry = new stdClass;
-    $entry->id = null;
-}
- 
+$itemid = 0; // This is used to distinguish between multiple file areas, e.g. different student's assignment submissions, or attachments to different forum posts, in this case we use '0' as there is no relevant id to use
+
+// Fetches the file manager draft area, called 'attachments' 
 $draftitemid = file_get_submitted_draft_itemid('attachments');
 
-file_prepare_draft_area($draftitemid, $context->id, 'local_filemanager', 'attachment', $entry->id, array('subdirs' => 0, 'maxbytes' => '0', 'maxfiles' => 50));
- 
-$entry->attachments = $draftitemid;
- 
+// Copy all the files from the 'real' area, into the draft area
+file_prepare_draft_area($draftitemid, $context->id, 'local_filemanager', 'attachment', $itemid, $filemanageropts);
+
+// Prepare the data to pass into the form - normally we would load this from a database, but, here, we have no 'real' record to load
+$entry = new stdClass();
+$entry->attachments = $draftitemid; // Add the draftitemid to the form, so that 'file_get_submitted_draft_itemid' can retrieve it
+// --------- 
+
 
 // Set form data
+// This will load the file manager with your previous files
 $mform->set_data($entry);
 
 
@@ -116,31 +89,35 @@ $mform->set_data($entry);
 // ===============
 //
 //
-//	PAGE OUTPUT
+// PAGE OUTPUT
 //
 //
 // ===============
-echo $OUTPUT->header('File Manager', 'File Manager', build_navigation(array(array('name'=>'File Manager','link'=>'','type'=>'misc'))));
+echo $OUTPUT->header();
 
+echo "<a href='/local/filemanager/index.php'><input type='button' value='Manage Files'></a>";
+echo "<a style='padding-left:10px' href='/local/filemanager/view.php'><input type='button' value='View Files'></a>";
 
 // ----------
 // Form Submit Status
 // ----------
-
-if ($mform->is_cancelled()) { 
-	// CANCELLED
-	echo '<h1>Cancelled</h1>';
+if ($mform->is_cancelled()) {
+    // CANCELLED
+    echo '<h1>Cancelled</h1>';
     echo '<p>Handle form cancel operation, if cancel button is present on form<p>';
-} else if ($fromform = $mform->get_data()) {
-	 // SUCCESS
-	echo '<h1>Success!</h1>';
-	echo '<p>In this case you process validated data. $mform->get_data() returns data posted in form.<p>';
-	echo "<p><center><a href='$CFG->wwwroot/local/filemanager'>Click here to return and see your File Manager!</a></center><p>";
-} else { 
-	// FAIL / DEFAULT
-	echo '<h1>Default / Fail</h1>';
-	echo '<p>This is the form first display OR "errors"<p>';
-	$mform->display();
+	echo '<a href="/local/filemanager/index.php"><input type="button" value="Try Again" /><a>';
+} else if ($data = $mform->get_data()) {
+    // SUCCESS
+    echo '<h1>Success!</h1>';
+    echo '<p>In this case you process validated data. $mform->get_data() returns data posted in form.<p>';
+
+    // Save the files submitted
+    file_save_draft_area_files($draftitemid, $context->id, 'local_filemanager', 'attachment', $itemid, $filemanageropts);
+} else {
+    // FAIL / DEFAULT
+    echo '<h1 style="text-align:center">Display form</h1>';
+    echo '<p>This is the form first display OR "errors"<p>';
+    $mform->display();
 }
 
 
---
+++ view.php
@@ -0,0 +1,64 @@
+<?php
+
+// --------------
+//
+// Standard Moodle Setup
+//
+// --------------
+require_once( '../../config.php' );
+global $CFG, $USER, $DB, $OUTPUT, $PAGE;
+
+$PAGE->set_url('/local/filemanager/index.php');
+require_login();
+
+$PAGE->set_pagelayout( 'admin' );
+
+// Choose the most appropriate context for your file manager - e.g. block, course, course module, this example uses
+// the system context (as we are in a 'local' plugin without any other context)
+// This is VERY important, the filemanager MUST have a valid context!
+$context = context_system::instance();
+$PAGE->set_context( $context );
+
+// Setup the page
+$PAGE->set_title( 'File Manager Example' );
+$PAGE->set_heading( 'File Manager Example' );
+
+//DEFINITIONS
+require_once($CFG->libdir.'/formslib.php');
+require_once('lib.php');
+
+
+// ===============
+//
+//
+// PAGE OUTPUT
+//
+//
+// ===============
+echo $OUTPUT->header();
+
+echo "<a href='/local/filemanager/index.php'><input type='button' value='Manage Files'></a>";
+echo "<a style='padding-left:10px' href='/local/filemanager/view.php'><input type='button' value='View Files'></a>";
+echo "<br /><br /><br />";
+
+			
+// ---------
+// Display Managed Files!
+// ---------
+$fs = get_file_storage();
+if ($files = $fs->get_area_files($context->id, 'local_filemanager', 'attachment', '0', 'sortorder', false)) {
+
+	// Look through each file being managed
+	foreach ($files as $file) {
+		// Build the File URL. Long process! But extremely accurate.
+		$fileurl = moodle_url::make_pluginfile_url($file->get_contextid(), $file->get_component(), $file->get_filearea(), $file->get_itemid(), $file->get_filepath(), $file->get_filename());
+		
+		// Display the image
+		echo "<br /><img src='$fileurl' />";
+	}
+} else {
+	echo '<p>Please upload an image first</p>';
+}
+	
+	
+echo $OUTPUT->footer();
